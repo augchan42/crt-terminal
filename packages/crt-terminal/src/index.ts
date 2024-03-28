2a19fc073e5834acabdf0f5470c22fdbe8ac207f
Jeffrey Quesnelle <jq@jeffq.com>
2024-03-27 21:39:21 -0400

    add PrintableItem to public types

--- packages/crt-terminal/src/index.ts
+++ packages/crt-terminal/src/index.ts
@@ -2,6 +2,7 @@ import Terminal from './components/Terminal/Terminal';
 
 export { Terminal };
 export type { TerminalProps } from './components/Terminal/Terminal';
+export type { PrintableItem } from './API/printer';
 
 export * from './hooks/eventQueue/useEventQueue';
 export * from './API/sentence/sentence';
